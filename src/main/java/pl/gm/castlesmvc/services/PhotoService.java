6ff82f960bc14dfcf5b170f8782431afcc70a38a
Grzesiek1337 <gmiedzian@gmail.com>
2022-04-25 18:56:17 +0200

    added overrided methods in service

--- src/main/java/pl/gm/castlesmvc/services/PhotoService.java
+++ src/main/java/pl/gm/castlesmvc/services/PhotoService.java
@@ -6,30 +6,53 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 import pl.gm.castlesmvc.model.Photo;
 import pl.gm.castlesmvc.repositories.PhotoRepository;
-
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 @Service
 @Transactional
-public class PhotoService implements  IPhotoService {
-
+public class PhotoService implements IPhotoService {
     @Autowired
     private PhotoRepository photoRepository;
 
     @Override
-    public void savePhotoImage(Photo photoDTO, MultipartFile imageFile) throws Exception {
+    public void savePhotoImage(Photo photo, MultipartFile imageFile) throws Exception {
         Path currentPath = Paths.get(".");
         Path absolutePath = currentPath.toAbsolutePath();
-        photoDTO.setPath(absolutePath + "/src/main/resources/static/photos/");
+        photo.setPath(absolutePath + "/src/main/resources/static/photos/");
         byte[] bytes = imageFile.getBytes();
-        Path path = Paths.get(photoDTO.getPath() + imageFile.getOriginalFilename());
+        Path path = Paths.get(photo.getPath() + imageFile.getOriginalFilename());
         Files.write(path, bytes);
     }
 
     @Override
-    public void save(Photo photoDTO) {
-        photoRepository.save(photoDTO);
+    public List<Photo> listAll() {
+        return photoRepository.findAll();
+    }
+
+    @Override
+    public void save(Photo photo) {
+        photoRepository.save(photo);
+    }
+
+    @Override
+    public Photo get(long id) {
+        return photoRepository.getById(id);
+    }
+
+    @Override
+    public void update(Photo photo) {
+        photoRepository.save(photo);
+    }
+
+    @Override
+    public void delete(long id) {
+        photoRepository.deleteById(id);
+    }
+
+    public Photo getPhotoByFileName(String fileName) {
+        return photoRepository.getPhotoByFileName(fileName);
     }
 }
