48044a896e9cbcffd22afe7d64f5d9f8c1a4f7c5
Grzesiek1337 <gmiedzian@gmail.com>
2022-04-25 18:57:15 +0200

    added method to find photo by filename

--- src/main/java/pl/gm/castlesmvc/repositories/PhotoRepository.java
+++ src/main/java/pl/gm/castlesmvc/repositories/PhotoRepository.java
@@ -1,7 +1,12 @@
 package pl.gm.castlesmvc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import pl.gm.castlesmvc.model.Photo;
 
 public interface PhotoRepository extends JpaRepository<Photo, Long> {
+
+    @Query("SELECT p FROM Photo p WHERE p.fileName = :fileName")
+    Photo getPhotoByFileName(@Param("fileName") String fileName);
 }
