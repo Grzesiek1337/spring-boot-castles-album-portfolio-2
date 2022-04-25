a0ca31758d8d9df7c1dd68a4c7c82d5d84544e8e
Grzesiek1337 <gmiedzian@gmail.com>
2022-04-25 18:58:35 +0200

    added map method to get list of string representing all photos filenames

--- src/main/java/pl/gm/castlesmvc/mapper/PhotoMapper.java
+++ src/main/java/pl/gm/castlesmvc/mapper/PhotoMapper.java
@@ -3,6 +3,9 @@ package pl.gm.castlesmvc.mapper;
 import pl.gm.castlesmvc.dto.PhotoDTO;
 import pl.gm.castlesmvc.model.Photo;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class PhotoMapper {
 
     public static PhotoDTO mapPhotoToDto(Photo photo) {
@@ -10,5 +13,11 @@ public class PhotoMapper {
                 photo.getPath(),
                 photo.getFileName(),
                 photo.getCastle());
+    }
+
+    public static List<String> mapToListOfPhotosNames(List<Photo> photos) {
+        return photos.stream()
+                .map(photo -> photo.getFileName())
+                .collect(Collectors.toList());
     }
 }
